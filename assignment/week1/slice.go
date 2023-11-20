a99db1a945a76d6a5d2566a3dbaa3685ada25454
harmonic-lqw <923377580@qq.com>
2023-11-20 18:52:58 +0800

    assignment week1 DeleteSlice

--- assignment/week1/slice.go
+++ assignment/week1/slice.go
@@ -1,5 +1,9 @@
 package week1
 
+const (
+	DEFAULT_REDUCE_CAPACITY_PERCENT = 0.25
+)
+
 type order interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint |
 		~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
@@ -21,5 +25,24 @@ func Insert[T order](idx int, val T, vals []T) []T {
 		}
 	}
 	vals[idx] = val
+	return vals
+}
+
+// Delete week1 assignment
+func Delete[T order](idx int, vals []T) []T {
+	if idx < 0 || idx >= len(vals) {
+		panic("idx not illegal")
+	}
+	vals = append(vals[:idx], vals[idx+1:]...)
+
+	// Reduce capacity when slice length is less than 0.25 of capacity.
+	// Reduced to 2 times the current length
+	lenVals := len(vals)
+	if float64(lenVals)/float64(cap(vals)) <= DEFAULT_REDUCE_CAPACITY_PERCENT {
+		newVals := make([]T, 0, 2*lenVals)
+		// newVals = vals
+		newVals = append(newVals, vals...)
+		return newVals
+	}
 	return vals
 }
--- assignment/week1/slice_test.go
+++ assignment/week1/slice_test.go
@@ -9,14 +9,45 @@ func TestInsert(t *testing.T) {
 	srcSlice := []int{1, 2}
 	tarSlice := Insert[int](0, 999, srcSlice)
 	if !reflect.DeepEqual(tarSlice, []int{999, 1, 2}) {
-		t.Log("failed")
+		t.Fatal("failed")
 	}
 	tarSlice = Insert[int](1, 999, srcSlice)
 	if !reflect.DeepEqual(tarSlice, []int{1, 999, 2}) {
-		t.Log("failed")
+		t.Fatal("failed")
 	}
 	tarSlice = Insert[int](2, 999, srcSlice)
 	if !reflect.DeepEqual(tarSlice, []int{1, 2, 999}) {
-		t.Log("failed")
+		t.Fatal("failed")
 	}
+}
+
+func TestDelete(t *testing.T) {
+	srcSlice := []int{1, 2, 3, 4, 5, 6}
+	tarSlice := Delete[int](0, srcSlice)
+	if !reflect.DeepEqual(tarSlice, []int{2, 3, 4, 5, 6}) {
+		t.Fatal("failed")
+	}
+
+	srcSlice = []int{1, 2, 3, 4, 5, 6}
+	tarSlice = Delete[int](len(srcSlice)-1, srcSlice)
+	if !reflect.DeepEqual(tarSlice, []int{1, 2, 3, 4, 5}) {
+		t.Log(tarSlice)
+		t.Fatal("failed")
+	}
+
+	srcSlice = []int{1, 2, 3, 4, 5, 6}
+	tarSlice = Delete[int](3, srcSlice)
+	if !reflect.DeepEqual(tarSlice, []int{1, 2, 3, 5, 6}) {
+		t.Fatal("failed")
+	}
+
+	srcSlice = []int{1, 2, 3, 4, 5, 6}
+	tarSlice = srcSlice
+	for i := 0; i < 5; i++ {
+		tarSlice = Delete[int](0, tarSlice)
+	}
+	if cap(tarSlice) != 2 {
+		t.Fatal("reduce capacity failed")
+	}
+	println(len(tarSlice), cap(tarSlice))
 }
